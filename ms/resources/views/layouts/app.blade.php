e9c9cb4fbe52dc277af19c17b7c5401af843097d
MaciejSzelag <56941288+MaciejSzelag@users.noreply.github.com>
2020-08-23 11:53:46 +0100

    project init

---
+++ ms/resources/views/layouts/app.blade.php
@@ -0,0 +1,86 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    {{-- <title>{{ config('app.name', 'Laravel') }}</title> --}}
+    @isset($title)
+        {{ $title }}
+    @endisset
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div id="app">
+        <nav class="nav-wrap">
+            <div class="container">
+                <div class="btn-home">
+                    <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'Maciej Szelag') }}
+                    </a>
+                </div>
+             
+
+
+                <div class="m-nav-container" >
+                    <!-- Left Side Of Navbar -->
+                    <ul class="main-m nav-items">
+                        <li class="nav-item"><a href="{{ url('/') }}">Home</a></li>
+                        <li class="nav-item"><a href="#about-me">About me</a></li>
+                        <li class="nav-item"><a href="#contact">Contact</a></li>
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="log-m nav-items">
+                        <!-- Authentication Links -->
+                        @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+</body>
+</html>
